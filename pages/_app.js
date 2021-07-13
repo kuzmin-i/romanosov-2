4bcb15e0a7992b1f7eb83d41d9a07148e8759ce4
Ilya Kuzmin <68997547+kuzmin-i@users.noreply.github.com>
2021-07-13 18:07:20 +0300

    cc

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import styles from '../index.css'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
